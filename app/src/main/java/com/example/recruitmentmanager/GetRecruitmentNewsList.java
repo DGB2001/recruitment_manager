2ea2690b669feb8c4a70af6dac7ae2565035d487
duogwas <duong2001n@gmail.com>
2023-04-14 11:09:17 +0700

    Feature: Get Recruitment News List
    Giao dien

---
+++ app/src/main/java/com/example/recruitmentmanager/GetRecruitmentNewsList.java
@@ -0,0 +1,14 @@
+package com.example.recruitmentmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetRecruitmentNewsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_recruitment_news_list);
+    }
+}
