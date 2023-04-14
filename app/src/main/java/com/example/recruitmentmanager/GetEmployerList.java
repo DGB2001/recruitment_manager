7b7692298d1ed5df89c18ead81e4655926cc05c4
duogwas <duong2001n@gmail.com>
2023-04-14 10:23:37 +0700

    Feature: Get Employer List

---
+++ app/src/main/java/com/example/recruitmentmanager/GetEmployerList.java
@@ -0,0 +1,14 @@
+package com.example.recruitmentmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetEmployerList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_employer_list);
+    }
+}
